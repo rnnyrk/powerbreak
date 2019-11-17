e8915eb3c686d9f3172be5f3d8da1702de8967b1
Ronny Rook <info@ronnyrook.nl>
2019-11-17 10:53:27 +0100

    Move detox config to backup file

---
+++ detox-bu.js
@@ -0,0 +1,15 @@
+// "detox": {
+//   "test-runner": "jest",
+//   "configurations": {
+//     "ios.release": {
+//       "binaryPath": "./ios/build/Build/Products/Test - Debug-iphonesimulator/powerbreak.app",
+//       "build": "xcodebuild -workspace ios/powerbreak.xcworkspace -configuration \"Test - Debug\" -scheme \"powerbreak - Test\" -sdk iphonesimulator -derivedDataPath ios/build",
+//       "type": "ios.simulator",
+//       "name": "iPhone 11"
+//     }
+//   }
+// }
+// "detox": "14.7.1",
+
+// "e2e:test": "detox test",
+// "e2e:build": "detox build"
