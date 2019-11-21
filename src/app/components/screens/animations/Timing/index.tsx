2082636ce012dfd8cf042ba893071bb0e773c1b0
Ronny Rook <info@ronnyrook.nl>
2019-11-21 23:07:46 +0100

    Setup basic timing interpolate

--- src/app/components/screens/animations/Timing/index.tsx
+++ src/app/components/screens/animations/Timing/index.tsx
@@ -3,17 +3,30 @@ import Animated from 'react-native-reanimated';
 
 import { TimingContainer, Bubble } from './styled';
 
-const { Value } = Animated;
+const { Value, interpolate, Extrapolate, useCode, block } = Animated;
 
 const Timing: React.FC<TimingProps> = () => {
   const progress = new Value(0);
 
+  const bubbles = [0, 1, 2];
+  const delta = 1 / bubbles.length;
+
   return (
     <TimingContainer>
-      {Array.from({ length: 3 }).map((_, index) => {
+      {bubbles.map((index) => {
+        const start = index * delta;
+        const end = start + delta;
+
+        const scale = interpolate(progress, {
+          inputRange: [start, end],
+          outputRange: [1, 1.5],
+          extrapolate: Extrapolate.CLAMP,
+        });
+
         return (
           <Bubble
             key={`bubble_${index}`}
+            style={{ transform: [{ scale }]}}
           />
         )
       })}
