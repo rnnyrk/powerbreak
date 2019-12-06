5a93cbe3f4c8b9dfa3303d7571a60393b61e1b7e
Ronny <ronny.rook@labela.nl>
2019-12-06 14:26:38 +0100

    Change import order

--- src/app/components/screens/general/index.ts
+++ src/app/components/screens/general/index.ts
@@ -1,3 +1,3 @@
 export { default as Dashboard } from './Dashboard';;
-export { default as Login } from './Login';
 export { default as Faq } from './Faq';
+export { default as Login } from './Login';
