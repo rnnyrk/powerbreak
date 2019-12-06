ea0453ea756bfca83ac2d5bc1cad6faa02c224a9
Ronny <ronny.rook@labela.nl>
2019-12-06 11:58:38 +0100

    Fix Accordion width issue

--- src/app/components/common/interaction/Accordion/styled.ts
+++ src/app/components/common/interaction/Accordion/styled.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { TextContent } from 'common/typography';
 
 export const AccordionContainer = styled.View`
-  width: 100%;
+  min-width: 100%;
   margin-bottom: 16px;
   border-radius: 4px;
   overflow: hidden;
