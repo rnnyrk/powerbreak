d7573791ec5015b3b0658644ec0cf1e575d50daf
Ronny <ronny.rook@labela.nl>
2019-12-06 17:53:09 +0100

    Start PanGesture screen

--- src/app/components/screens/animations/PanGesture/index.tsx
+++ src/app/components/screens/animations/PanGesture/index.tsx
@@ -1,17 +1,67 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { ViewComponent } from 'react-native';
 import Animated, { Easing } from 'react-native-reanimated';
-import { useMemoOne } from 'use-memo-one';
-
-import { Container } from 'common/general';
-import { TextContent } from 'common/typography';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import { onGestureEvent, clamp } from 'react-native-redash';
 
 import { CardContainer, Card } from './styled';
 
+const CARD_WIDTH = 200;
+const CARD_HEIGHT = 200;
+
+const { Value, block, cond, eq, set, add } = Animated;
+
+const withOffset = (value: Animated.Value<number>, state: Animated.Value<State>) => {
+  const offset = new Value(0); // last position of the card
+
+  return cond(
+    eq(state, State.END),
+    [set(offset, add(offset, value)), offset],
+    add(offset, value),
+  );
+};
+
 const PanGesture: React.FC<PanGestureProps> = () => {
+  const ref = useRef<ViewComponent>(null);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const state = new Value(State.UNDETERMINED);
+  const translationX = new Value(0);
+  const translationY = new Value(0);
+
+  // onGestureEvent from redash is a shortcut to event from gesture-handler
+  // and prevents you from having to set onGestureEvent and
+  // onHandlerStateChange on the PanGestureHandler
+  const gestureHandler = onGestureEvent({
+    state,
+    translationY,
+    translationX,
+  });
+
+  useEffect(() => {
+    if (ref.current) {
+      console.log('ref', ref.current);
+    }
+  }, [ref]);
+
+  // get CardContainer height and width
+  const containerWidth = 0;
+  const containerHeight = 0;
+
+  const translateX = clamp(withOffset(translationX, state), 0, containerWidth - CARD_WIDTH);
+  const translateY = clamp(withOffset(translationY, state), 0, containerHeight - CARD_HEIGHT);
+
   return (
-    <Container>
-      <TextContent>PanGesture</TextContent>
-    </Container>
+    <CardContainer ref={ref}>
+      <PanGestureHandler {...gestureHandler}>
+        <Animated.View style={{
+          transform: [{ translateX }, { translateY }],
+        }}>
+          <Card />
+        </Animated.View>
+      </PanGestureHandler>
+    </CardContainer>
   );
 };
 
--- src/app/components/screens/animations/PanGesture/styled.ts
+++ src/app/components/screens/animations/PanGesture/styled.ts
@@ -1,19 +1,15 @@
 import Animated from 'react-native-reanimated';
 import styled from 'styled-components';
 
-export const CardContainer = styled(Animated.View)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  justify-content: center;
-  align-items: center;
+import { Container } from 'common/general';
+
+export const CardContainer = styled(Container)`
+  padding: 0;
 `;
 
 export const Card = styled.View`
-  width: 100%;
+  width: 200px;
   height: 200px;
-  border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors.blue.light};
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.blue.default};
 `;
