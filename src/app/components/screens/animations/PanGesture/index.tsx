490df33e7d65f017dcf31bfa43bf2306663cfe87
Ronny <ronny.rook@labela.nl>
2019-12-12 17:33:42 +0100

    Fix PanGesture animation

--- src/app/components/screens/animations/PanGesture/index.tsx
+++ src/app/components/screens/animations/PanGesture/index.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { View } from 'react-native';
-import Animated, { Easing } from 'react-native-reanimated';
+import React from 'react';
+import { Dimensions } from 'react-native';
+import Animated from 'react-native-reanimated';
 import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import { onGestureEvent, clamp } from 'react-native-redash';
 
@@ -9,7 +9,11 @@ import { CardContainer, Card } from './styled';
 const CARD_WIDTH = 200;
 const CARD_HEIGHT = 200;
 
-const { Value, block, cond, eq, set, add } = Animated;
+const { width, height } = Dimensions.get('window');
+const containerWidth = width;
+const containerHeight = height;
+
+const { Value, cond, eq, set, add, diffClamp } = Animated;
 
 const withOffset = (value: Animated.Value<number>, state: Animated.Value<State>) => {
   const offset = new Value(0); // last position of the card
@@ -22,9 +26,6 @@ const withOffset = (value: Animated.Value<number>, state: Animated.Value<State>)
 };
 
 const PanGesture: React.FC<PanGestureProps> = () => {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-
   const state = new Value(State.UNDETERMINED);
   const translationX = new Value(0);
   const translationY = new Value(0);
@@ -38,27 +39,11 @@ const PanGesture: React.FC<PanGestureProps> = () => {
     translationX,
   });
 
-  useEffect(() => {
-
-  }, [])
-
-
-  // get CardContainer height and width
-  console.log(width, height);
-  const containerWidth = width;
-  const containerHeight = height;
-
-  const translateX = clamp(withOffset(translationX, state), 0, containerWidth - CARD_WIDTH);
-  const translateY = clamp(withOffset(translationY, state), 0, containerHeight - CARD_HEIGHT);
+  const translateX = diffClamp(withOffset(translationX, state), -85, (containerWidth - CARD_WIDTH - 85));
+  const translateY = diffClamp(withOffset(translationY, state), -211, (containerHeight - CARD_HEIGHT - 401));
 
   return (
-    <CardContainer
-      onLayout={(event) => {
-        const { width, height } = event.nativeEvent.layout;
-        setWidth(width);
-        setHeight(height);
-      }}
-    >
+    <CardContainer>
       <PanGestureHandler {...gestureHandler}>
         <Animated.View style={{
           transform: [{ translateX }, { translateY }],
