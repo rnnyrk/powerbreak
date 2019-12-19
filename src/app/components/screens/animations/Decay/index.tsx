dd144a30ea2257ee217d3b65eb53f8d59d3ca886
Ronny Rook <info@ronnyrook.nl>
2019-12-19 18:12:16 +0100

    Add Decay folder

---
+++ src/app/components/screens/animations/Decay/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Animated, { Easing } from 'react-native-reanimated';
+
+import { DecayContainer } from './styled';
+
+const Decay: React.FC<DecayProps> = () => {
+  return (
+    <DecayContainer>
+    </DecayContainer>
+  );
+};
+
+type DecayProps = {};
+
+export default Decay;
---
+++ src/app/components/screens/animations/Decay/styled.ts
@@ -0,0 +1,8 @@
+import Animated from 'react-native-reanimated';
+import styled from 'styled-components';
+
+import { Container } from 'common/general';
+
+export const DecayContainer = styled(Container)`
+  padding: 0;
+`;
